d745568523e05b75787c7ffbc3fe60e89c50923d
Exanlv <exan@landviz.nl>
2023-07-03 15:43:06 +0200

    Implement league/route

--- public/index.php
+++ public/index.php
@@ -2,15 +2,14 @@
 
 use Exan\Config\Config;
 use Exan\Container\Container;
-use Exan\Landviz\Controllers\ErrorController;
-use Exan\Landviz\Routes;
-use Exan\Router\Exceptions\HttpException;
-use Exan\Router\Exceptions\HttpServerException;
-use Exan\Router\Router;
+use Exan\Landviz\Controllers\FuckController;
+use Exan\Landviz\Controllers\HomeController;
 use HttpSoft\Emitter\SapiEmitter;
 use HttpSoft\ServerRequest\ServerRequestCreator;
 use League\Plates\Engine;
-use Psr\Http\Message\ResponseInterface;
+use League\Route\Router;
+use League\Route\Strategy\ApplicationStrategy;
+use League\Route\Strategy\StrategyInterface;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -26,18 +25,19 @@ $container = new Container();
 $container->register(Engine::class, new Engine(__DIR__ . '/../templates'));
 $container->register(Config::class, new Config(__DIR__ . '/../conf'));
 
-$router = new Router($container, Routes::get());
-$request = ServerRequestCreator::createFromGlobals();
+/** @var StrategyInterface */
+$strategy = (new ApplicationStrategy())->setContainer($container);
+/** @var Router */
+$router = (new Router())->setStrategy($strategy);
 
-try {
-    /** @var ResponseInterface */
-    $response = $router->run($request);
-} catch (HttpException $e) {
-    $response = $container->get(ErrorController::class)->render($e);
-} catch (Throwable $e) {
-    $response = $container->get(ErrorController::class)->render(
-        new HttpServerException(previous: $e),
-    );
-}
+$router->map('GET', '/',         [HomeController::class, 'index']);
+$router->map('GET', '/projects', [HomeController::class, 'projects']);
+$router->map('GET', '/colors',   [HomeController::class, 'colors']);
+
+$router->map('GET',  '/fuck', [FuckController::class, 'form']);
+$router->map('POST', '/fuck', [FuckController::class, 'fuck']);
+
+$request = ServerRequestCreator::createFromGlobals();
+$response = $router->dispatch($request);
 
 (new SapiEmitter())->emit($response);
